2895b1ff7ef5702289ad422081891e1efb9f282c
Devon Truman <dltruman2012@gmail.com>
2015-05-22 13:37:46 -0600

    Service for Board Generation more Setup

---
+++ Board Game Tool/Collection Game Tool/Services/Tile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection_Game_Tool.Services
+{
+    interface Tile
+    {
+        List<Tile> nodes;
+
+        //Talan you should probably flesh this out how you want it
+        //I doubt you will want to override this each time
+        public void addTile(Tile tile);
+
+        //Again change this how you need it
+        public void tileAction();
+    }
+}
