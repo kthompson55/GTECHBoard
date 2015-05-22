8b1e788bcf403744a1fd06197f286cccf05fe15c
Devon Truman <dltruman2012@gmail.com>
2015-05-22 13:21:02 -0600

    Board Stuff Setup

---
+++ Board Game Tool/Collection Game Tool/Services/GamePlayGeneration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection_Game_Tool.Services
+{
+    class GamePlayGeneration
+    {
+        private List<Board> boards;
+
+        GamePlayGeneration(List<Board> boards)
+        {
+            this.boards = boards;
+        }
+
+        private void GeneratePlaysFromBoard(Board board)
+        {
+
+        }
+
+        private void Generate()
+        {
+            foreach (Board b in boards)
+            {
+                GeneratePlaysFromBoard(b);
+            }
+        }
+    }
+}
