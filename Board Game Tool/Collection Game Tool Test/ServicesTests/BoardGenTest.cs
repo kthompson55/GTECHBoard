957b0a6a3558614faf483423bc459ffa44d6050c
Matthew Radcliffe <mradcliffe@student.neumont.edu>
2015-07-17 13:19:56 -0600

    Ensure First Tile is Blank

--- Board Game Tool/Collection Game Tool Test/ServicesTests/BoardGenTest.cs
+++ Board Game Tool/Collection Game Tool Test/ServicesTests/BoardGenTest.cs
@@ -15,73 +15,75 @@ namespace Collection_Game_Tool_Test.ServicesTests
         [TestMethod]
         public void personalGenBoardTest()
         {
-            BoardGeneration bg = new BoardGeneration();
-            int boardSize = 26;
-            int initialReachable = 20;
-            PrizeLevels prizes = new PrizeLevels();
-            PrizeLevel A1 = new PrizeLevel();
-            A1.numCollections = 3;
-            A1.prizeLevel = 1;
-            A1.isBonusGame = true;
-            prizes.addPrizeLevel(A1);
 
-            PrizeLevel A2 = new PrizeLevel();
-            A2.numCollections = 3;
-            A2.prizeLevel = 2;
-            prizes.addPrizeLevel(A2);
-
-            PrizeLevel A3 = new PrizeLevel();
-            A3.numCollections = 3;
-            A3.prizeLevel = 3;
-            A3.isInstantWin = true;
-            prizes.addPrizeLevel(A3);
-
-            PrizeLevel A4 = new PrizeLevel();
-            A4.numCollections = 3;
-            A4.prizeLevel = 4;
-            prizes.addPrizeLevel(A4);
-
-            int numberOfExpectedCollection = 12;
-            int numberOfExpectedMoveBack = 3;
-            int numberOfExpectedMoveForward = 2;
-            ITile firstTile = bg.GenerateBoard(boardSize, initialReachable, 2, 6, numberOfExpectedMoveBack, numberOfExpectedMoveForward, prizes, 1, 2);
-
-            int numberOfCollection = 0;
-            int numberOfMoveBack = 0;
-            int numberOfMoveForward = 0;
-            int numberOfextraGames = 0;
-            int numberOfTiles = 0;
-            ITile currentTile = firstTile;
-            while (currentTile != null)
+            for (int i = 0; i < 1000; i++)
             {
-                if (currentTile.Type == TileTypes.collection)
-                {
-                    numberOfCollection++;
-                }
-                else if (currentTile.Type == TileTypes.moveForward)
-                {
-                    numberOfMoveForward++;
-                }
-                else if (currentTile.Type == TileTypes.moveBack)
-                {
-                    numberOfMoveBack++;
-                }
-                else if (currentTile.Type == TileTypes.extraGame)
-                {
-                    numberOfextraGames++;
-                }
-                numberOfTiles++;
-                currentTile = currentTile.Child;
-            }
-            string board = bg.ToString();
-            Assert.IsTrue(numberOfCollection == numberOfExpectedCollection, "Number Of Collection Tiles not correct");
-            Assert.IsTrue(numberOfMoveBack == numberOfExpectedMoveBack, "Number Of MoveBack Tiles not correct");
-            Assert.IsTrue(numberOfMoveForward == numberOfExpectedMoveForward, "Number Of MoveForward Tiles not correct");
-            Assert.IsTrue(boardSize == numberOfTiles, "Number Of Tiles not correct");
-            if (board == "")
-            {
+                BoardGeneration bg = new BoardGeneration();
+                int boardSize = 26;
+                int initialReachable = 20;
+                PrizeLevels prizes = new PrizeLevels();
+                PrizeLevel A1 = new PrizeLevel();
+                A1.numCollections = 3;
+                A1.prizeLevel = 1;
+                A1.isBonusGame = true;
+                prizes.addPrizeLevel(A1);
 
+                PrizeLevel A2 = new PrizeLevel();
+                A2.numCollections = 3;
+                A2.prizeLevel = 2;
+                prizes.addPrizeLevel(A2);
+
+                PrizeLevel A3 = new PrizeLevel();
+                A3.numCollections = 3;
+                A3.prizeLevel = 3;
+                A3.isInstantWin = true;
+                prizes.addPrizeLevel(A3);
+
+                PrizeLevel A4 = new PrizeLevel();
+                A4.numCollections = 3;
+                A4.prizeLevel = 4;
+                prizes.addPrizeLevel(A4);
+
+                int numberOfExpectedCollection = 12;
+                int numberOfExpectedMoveBack = 3;
+                int numberOfExpectedMoveForward = 2;
+                ITile firstTile = bg.GenerateBoard(boardSize, initialReachable, 2, 6, numberOfExpectedMoveBack, numberOfExpectedMoveForward, prizes, 1, 2);
+
+                int numberOfCollection = 0;
+                int numberOfMoveBack = 0;
+                int numberOfMoveForward = 0;
+                int numberOfextraGames = 0;
+                int numberOfTiles = 0;
+                ITile currentTile = firstTile;
+                while (currentTile != null)
+                {
+                    if (currentTile.Type == TileTypes.collection)
+                    {
+                        numberOfCollection++;
+                    }
+                    else if (currentTile.Type == TileTypes.moveForward)
+                    {
+                        numberOfMoveForward++;
+                    }
+                    else if (currentTile.Type == TileTypes.moveBack)
+                    {
+                        numberOfMoveBack++;
+                    }
+                    else if (currentTile.Type == TileTypes.extraGame)
+                    {
+                        numberOfextraGames++;
+                    }
+                    numberOfTiles++;
+                    currentTile = currentTile.Child;
+                }
+                string board = bg.ToString();
+                Assert.IsTrue(numberOfCollection == numberOfExpectedCollection, "Number Of Collection Tiles not correct");
+                Assert.IsTrue(numberOfMoveBack == numberOfExpectedMoveBack, "Number Of MoveBack Tiles not correct");
+                Assert.IsTrue(numberOfMoveForward == numberOfExpectedMoveForward, "Number Of MoveForward Tiles not correct");
+                Assert.IsTrue(boardSize == numberOfTiles, "Number Of Tiles not correct");
+                Assert.IsTrue(firstTile.Type == TileTypes.blank);
             }
+
         }
     }
 }
